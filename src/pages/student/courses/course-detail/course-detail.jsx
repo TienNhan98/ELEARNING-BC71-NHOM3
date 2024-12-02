859a24fcd71fde9a50a2a3ba34dfe72356050979
ChieuHuynhIT <chieuhuynhit@gmail.com>
2024-12-02 15:43:02 +0700

    update khoa hoc tham khao o detail

--- src/pages/student/courses/course-detail/course-detail.jsx
+++ src/pages/student/courses/course-detail/course-detail.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import styles from "./DetailPage.module.scss";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { callApiKhoaHoc } from "../../../../service/callApiKhoaHoc";
 
 import Swal from "sweetalert2";
+import { Card, Col, Popover, Row } from "antd";
+import avtELM from "../../list-courses-by-cat/asset/avaElonmuck.png";
+import avtPPV from "../../list-courses-by-cat/asset/avaPPV.png";
 
 export default function CourseDetail() {
   const navigate = useNavigate();
   const { maKhoaHoc } = useParams();
-
+  const [listKhoaHoc, setListKhoaHoc] = useState([]);
   const [courseDetail, setCourseDetail] = useState([]);
 
+  useEffect(() => {
+    callApiKhoaHoc
+      .layDanhSachKhoaHoc()
+      .then((result) => {
+        // console.log(result.data);
+        setListKhoaHoc(result.data);
+      })
+      .catch((err) => {});
+  }, []);
+
+  const khoaHocThamKhao = listKhoaHoc.slice(10, 14);
   // hàm btn đăng ký khóa học
   const btnRegisterCourse = () => {
     const userLogin = JSON.parse(localStorage.getItem("USER_LOGIN"));
@@ -417,6 +431,202 @@ export default function CourseDetail() {
             </div>
           </div>
         </div>
+      </div>
+
+      {/* khoaHocThamKhao */}
+      <a href="#" className="no-underline inline-block ml-5 mt-3">
+        <p
+          className="font-bold"
+          style={{
+            color: "#000000",
+          }}
+        >
+          Khóa học tham khảo
+        </p>
+      </a>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 justify-center">
+        {khoaHocThamKhao.map((item) => {
+          const popoVerKhoaHocThamKhao = (
+            <div>
+              <Card style={{ width: 300 }}>
+                <div className="flex items-center">
+                  <img src={avtPPV} width={"40px"} alt="" />
+                  <span className="ml-2 font-bold" style={{ color: "#8C8C8C" }}>
+                    Elun Musk Ricard
+                  </span>
+                </div>
+                <div className="my-3">
+                  <p className="font-bold">
+                    BOOTCAMP - LẬP TRÌNH FULL STACK TỪ ZERO ĐẾN CÓ VIỆC
+                  </p>
+                </div>
+                <div className="mb-3" style={{ color: "#8C8C8C" }}>
+                  <p>
+                    Đã có hơn 6200 bạn đăng kí học và có việc làm thông qua
+                    chương trình đào tạo Bootcamp Lập trình Front End chuyên
+                    nghiệp. Khóa học 100% thực hành cường độ cao theo dự án thực
+                    tế và kết nối doanh nghiệp hỗ trợ tìm việc ngay sau khi
+                    học...
+                  </p>
+                </div>
+                <div
+                  className="flex justify-between items-center mb-3 space-x-2"
+                  style={{ fontSize: "16px", color: "#8c8c8c" }}
+                >
+                  <span>
+                    <i
+                      className="far fa-clock mr-1"
+                      style={{ color: "#f5c002" }}
+                    ></i>
+                    8 giờ
+                  </span>
+                  <span>
+                    <i
+                      className="far fa-calendar-alt mr-1"
+                      style={{ color: "#f06f68" }}
+                    ></i>
+                    4 Tuần
+                  </span>
+                  <span>
+                    <i
+                      className="fas fa-signal mr-1"
+                      style={{ color: "#65c9ff" }}
+                    ></i>
+                    Tất Cả
+                  </span>
+                </div>
+                <div className="mt-3">
+                  <NavLink
+                    className="no-underline block w-full h-full bg-[#41b294] text-white text-center py-3"
+                    to={`/chitiet/${item.maKhoaHoc}`}
+                  >
+                    Xem Chi Tiết
+                  </NavLink>
+                </div>
+              </Card>
+            </div>
+          );
+          return (
+            <div className="flex justify-center" key={item.maKhoaHoc}>
+              <Popover content={popoVerKhoaHocThamKhao}>
+                <Card
+                  className="relative"
+                  onClick={() => {
+                    navigate(`/chitiet/${item.maKhoaHoc}`);
+                  }}
+                  hoverable
+                  style={{
+                    width: 300,
+                    height: 400,
+                    padding: "10px",
+                  }}
+                  cover={
+                    <div className="h-[175px] overflow-hidden">
+                      <img
+                        alt="example"
+                        src={item.hinhAnh}
+                        className="w-full h-full object-fill"
+                      />
+                    </div>
+                  }
+                >
+                  <p
+                    className="inline-block text-center font-semibold px-2 absolute top-40 left-0"
+                    style={{
+                      backgroundColor: "#41b294",
+                      color: "#fff",
+                      fontSize: "16px",
+                    }}
+                  >
+                    {item.tenKhoaHoc}
+                  </p>
+                  <p className="font-bold mb-2" style={{ fontSize: "16px" }}>
+                    Lập Trình hiện đang là xu hướng trên toàn thế giới...
+                  </p>
+                  <div
+                    className="flex justify-between items-center mb-4 space-x-2"
+                    style={{
+                      fontSize: "16px",
+                      color: "#8c8c8c",
+                    }}
+                  >
+                    <span>
+                      <i
+                        className="far fa-clock mr-1"
+                        style={{
+                          color: "#f5c002",
+                        }}
+                      ></i>
+                      8 giờ
+                    </span>
+                    <span>
+                      <i
+                        className="far fa-calendar-alt mr-1"
+                        style={{ color: "#f06f68" }}
+                      ></i>
+                      4 Tuần
+                    </span>
+                    <span>
+                      <i
+                        className="fas fa-signal mr-1"
+                        style={{ color: "#65c9ff" }}
+                      ></i>
+                      Tất Cả
+                    </span>
+                  </div>
+                  <div className="mt-1 -mx-4 border-t-2 border-gray-300 pt-2">
+                    <Row className="pb-0 justify-between items-center">
+                      <Col span={8} className="flex justify-start items-center">
+                        <div className="flex items-center space-x-2">
+                          <img
+                            src={avtELM}
+                            style={{ width: "35px" }}
+                            alt=""
+                            className="border-t-2 border-b-2 border-[#f6ba35] p-1 rounded-full"
+                          />
+                          <span
+                            className="font-bold"
+                            style={{
+                              color: "#8C8C8C",
+                              whiteSpace: "nowrap",
+                            }}
+                          >
+                            Elon Musk
+                          </span>
+                        </div>
+                      </Col>
+                      <Col span={8} className="flex justify-end items-center">
+                        <div className="text-center">
+                          <p
+                            className="line-through mb-0 text-gray-500 text-sm"
+                            style={{ fontSize: "12px", marginBottom: "0" }}
+                          >
+                            800.000 <sup>đ</sup>
+                          </p>
+                          <p
+                            className="font-bold text-[#41b294]"
+                            style={{
+                              fontSize: "16px",
+                              marginBottom: "0",
+                              display: "flex",
+                              alignItems: "center",
+                            }}
+                          >
+                            400.000 <sup>đ</sup>
+                            <i
+                              className="fas fa-tag"
+                              style={{ color: "red", marginLeft: "5px" }}
+                            ></i>
+                          </p>
+                        </div>
+                      </Col>
+                    </Row>
+                  </div>
+                </Card>
+              </Popover>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
